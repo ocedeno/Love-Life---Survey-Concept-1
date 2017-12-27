71b277fc4e9714378395b5eba8ac5e16d497ad78
Octavio Cedeno <octavio.cedeno@me.com>
2017-12-27 13:45:42 -0500

    added additional senses view controller

---
+++ Love Life - Survey Concept 1/HearViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HearViewController.swift
+//  Love Life - Survey Concept 1
+//
+//  Created by Octavio Cedeno on 12/27/17.
+//  Copyright © 2017 Cedeno Enterprise, Inc. All rights reserved.
+//
+
+import UIKit
+
+class HearViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
